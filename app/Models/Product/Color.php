1b9fde54e470710bb78292f67a5c7ae9addd49dc
triadi <triadilaksamana@gmail.com>
2022-11-02 23:22:04 +0700

    add model, seeder color product

---
+++ app/Models/Product/Color.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Product;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Color extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "color"
+    ];
+}
