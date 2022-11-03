757bd54d5096c0dc6fb22674a490794adabf956a
triadi <triadilaksamana@gmail.com>
2022-11-03 08:38:10 +0700

    add user role model migration

---
+++ database/migrations/2022_11_03_013043_create_user_roles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->foreignId("role_id")->constrained("roles")->nullOnDelete();
+            $table->foreignId("user_id")->constrained("users")->nullOnDelete();
+            $table->primary(['role_id', 'user_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_roles');
+    }
+};
