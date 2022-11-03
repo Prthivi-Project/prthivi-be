76c3f56f8fe27f4e70569f2fc38628d42a792eef
triadi <triadilaksamana@gmail.com>
2022-11-03 08:44:47 +0700

    add user role relational

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -12,4 +12,9 @@ class Role extends Model
     protected $fillable = [
         'role'
     ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_roles');
+    }
 }
