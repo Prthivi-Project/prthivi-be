2cc8358e978228c1480dae59a3830a9f178145f3
triadi <triadilaksamana@gmail.com>
2022-11-03 11:27:12 +0700

    add jwt middleware

---
+++ app/Http/Middleware/JWTMiddleware.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Exceptions\AuthException;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Validation\UnauthorizedException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenBlacklistedException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenExpiredException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenInvalidException;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+class JWTMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (\Throwable $th) {
+            //throw $th;
+            if ($th instanceof TokenInvalidException) {
+                throw new AccessDeniedHttpException($th->getMessage());
+            } else if ($th instanceof TokenInvalidException) {
+                throw new UnauthorizedHttpException($th->getMessage());
+            } else if ($th instanceof TokenBlacklistedException) {
+                throw new BadRequestHttpException($th->getMessage());
+            } else {
+                throw new NotFoundHttpException($th->getMessage());
+            }
+        }
+        return $next($request);
+    }
+}
