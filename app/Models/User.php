96c1eddfe5dd93c3e4ca9e360a0b5679c51d113a
triadi <triadilaksamana@gmail.com>
2022-11-03 08:41:32 +0700

    fix store user relational

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    // relational
+    public function store()
+    {
+        return $this->hasMany(Store::class);
+    }
 }
