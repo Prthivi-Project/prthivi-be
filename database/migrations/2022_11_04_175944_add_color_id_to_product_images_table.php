c0cc53d24b418de1cfa4f199560c875df8631db4
triadi <triadilaksamana@gmail.com>
2022-11-05 12:25:34 +0700

    add field color id in product color

---
+++ database/migrations/2022_11_04_175944_add_color_id_to_product_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('product_images', function (Blueprint $table) {
+            $table->integer('priority_level')->default(1)->after('image_url');
+            $table->foreignId('color_id')->after('priority_level')->nullable()->constrained('colors')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('product_images', function (Blueprint $table) {
+            $table->dropColumn('priority_level');
+            $table->dropColumn('color_id');
+        });
+    }
+};
