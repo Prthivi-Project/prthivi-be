f6fa7693e1d04b9ffbf5177f3ef5db08fa600663
triadi <triadilaksamana@gmail.com>
2022-11-05 12:30:21 +0700

    add upload avatar in register + fix bug password

--- app/Http/Controllers/Auth/RegisterUserController.php
+++ app/Http/Controllers/Auth/RegisterUserController.php
@@ -5,23 +5,51 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
+use App\Traits\MediaUpload;
 use App\Traits\ResponseFormatter;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
+use Faker\Generator;
+
 class RegisterUserController extends Controller
 {
     use ResponseFormatter;
+    use MediaUpload;
+
+
+    private $dirName = 'avatar';
     //
     public function register(RegisterRequest $request)
     {
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        $payload = $request->safe()->except("password", 'avatar_file', 'avatar_base64');
+        $payload['role_id'] = 4; // customer
+        $avatar_file = $request->file("avatar_file");
+        $avatar_base64 = $request->input("avatar_base64");
+
+        if ($avatar_base64) {
+            $this->checkAndCreateDirIfNotExist(self::$dirName);
+            $filePath = $this->storeMediaAsBased64($avatar_base64, self::$dirName);
+            if (!$filePath) {
+                return $this->error(500, "Error while uploading avatar", null);
+            }
+            $payload['avatar'] = \asset('storage' . $filePath);
+        } elseif ($avatar_file) {
+            $this->checkAndCreateDirIfNotExist(self::$dirName);
+            $filePath = $this->storeMediaAsFile($avatar_file, self::$dirName);
+            if (!$filePath) {
+                return $this->error(500, "Error while uploading avatar", null);
+            }
+            $payload['avatar'] = \asset('storage' . $filePath);
+        } else {
+            $payload['avatar'] = \fake()->imageUrl();
+        }
+        $payload['password'] = Hash::make($request->password);
+
+
+        $user = User::create($payload);
 
         event(new Registered($user));
 
